7151394a6bb32ba18d90cea88c2f20e7835aed42
mridul-netizen <mridulsetia18@gmail.com>
2020-10-28 17:32:58 +0530

    new

--- js/index.js
+++ js/index.js
@@ -1,36 +1,37 @@
-//javascript for index.html
+// javascript for index.html
 const container = document.querySelector('.blogs');
 const searchForm = document.querySelector('.search');
 
-
 const renderPosts = async (term) => {
-    let uri = 'http://localhost:3000/posts?_sort=likes&_order=desc';//default sorting order is by id
+  console.log(term);
+  let uri = 'http://localhost:3000/posts?_sort=likes&_order=desc';
+  if (term) {
+    uri += `&q=${term}`
+  }
 
-    if(term) {
-      uri += `&q=${term}`;
-    }
-    const res = await fetch(uri);
+  const res = await fetch(uri);
+  const posts = await res.json();
+  console.log(posts);
 
-    const posts = await res.json();
+  let template = '';
+  posts.forEach(post => {
+    template += `
+      <div class="post">
+        <h2>${post.title}</h2>
+        <p><small>${post.likes} likes</small></p>
+        <p>${post.body.slice(0, 200)}...</p>
+        <a href="/details.html?id=${post.id}">Read more</a>
+      </div>
+    `
+  });
 
-    let template = '';
-    posts.forEach(post => {
-        template += `
-          <div class="post">
-            <h2>${post.title}</h2>
-            <p><small>${post.likes} likes</small></p>
-            <p>${post.body.slice(0,200)}</p>
-            <a href="/details.html?id=${post.id}">read more...</a>
-          </div>
-         `
-    });
-
-    container.innerHTML = template;
+  container.innerHTML = template;
 }
 
-searchForm.addEventListener('submit', (e) => {
+// search
+searchForm.addEventListener('submit', async (e) => {
   e.preventDefault();
-  renderPosts(searchForm.term.value.trim())
+  renderPosts(searchForm.term.value.trim());
 })
 
-window.addEventListener('DOMContentLoaded',() => renderPosts());
+window.addEventListener('DOMContentLoaded', () => renderPosts());
